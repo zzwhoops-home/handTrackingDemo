9c4fb1d9045000ef2ff8acb4c817ace0c9eca303
zzwhoops-home <zd@5rock.org>
2023-06-01 22:23:36 -0400

    Live webcam + add close window

--- hand_pose.py
+++ hand_pose.py
@@ -13,28 +13,42 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 # camera instance
 capture = cv2.VideoCapture(0)
+# 3 for frame width, 4 for frame height
+capture.set(3, 1280)
+capture.set(4, 720)
 
-while True:
-  success, img = capture.read()
+while capture.isOpened():
+    success, img = capture.read()
 
-  if not success:
-    break
-  cv2.imshow("Webcam", img)
-  cv2.waitKey(25)
+    if not success:
+        break
 
-# Create a hand landmarker instance with the live stream mode:
-def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-    print('hand landmarker result: {}'.format(result))
+    # print(result.multi_hand_landmarks)
+    cv2.imshow("Webcam Display", img)
+    cv2.waitKey(20)
 
-options = HandLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path='/path/to/model.task'),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
-
-with HandLandmarker.create_from_options(options) as landmarker:
-  # The landmarker is initialized. Use it here.
-  # ...
-  exit()
+    # unicode black magic
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
 
 capture.release()
 cv2.destroyAllWindows()
+
+# Create a hand landmarker instance with the live stream mode:
+def print_result(
+    result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int
+):
+    print("hand landmarker result: {}".format(result))
+
+
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path="hand_landmarker.task"),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result,
+)
+
+with HandLandmarker.create_from_options(options) as landmarker:
+    # The landmarker is initialized. Use it here.
+    # ...
+    exit()
+
