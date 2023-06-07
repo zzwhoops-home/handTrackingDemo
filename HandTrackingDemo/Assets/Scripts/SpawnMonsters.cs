89fec84e720b2c558aa91af8cf8c3ef69e48b4fe
zzwhoops-home <zd@5rock.org>
2023-06-07 01:00:36 -0400

    Add loops so more than one round spawns. oops

--- HandTrackingDemo/Assets/Scripts/SpawnMonsters.cs
+++ HandTrackingDemo/Assets/Scripts/SpawnMonsters.cs
@@ -8,6 +8,7 @@ public class SpawnMonsters : MonoBehaviour
 
     public GameObject[] monsters;
     private int round = 0;
+    public int numRounds = 10;
     private bool roundEnd = false;
 
     // Start is called before the first frame update
@@ -23,27 +24,31 @@ public class SpawnMonsters : MonoBehaviour
     }
     private IEnumerator Spawn() 
     {
-        round++;
-        for (int i = 0 ; i < round * 5; i++) {
-            // spawn from random spawnpoint in world
-            int spawn = Random.Range(0, 4);
-            Transform spawnPoint = spawnPoints[spawn].transform;
-            // spawn monster
-            Instantiate(monsters[0], spawnPoint.position, Quaternion.identity);
-            yield return new WaitForSeconds(0.25f);
+        for (int i = 0; i < numRounds; i++) {
+            round++;
+            for (int m = 0 ; m < round * 5; m++) {
+                // spawn from random spawnpoint in world
+                int spawn = Random.Range(0, 4);
+                Transform spawnPoint = spawnPoints[spawn].transform;
+                // spawn monster
+                Instantiate(monsters[0], spawnPoint.position, Quaternion.identity);
+                yield return new WaitForSeconds(0.25f);
+            }
+            yield return new WaitUntil(() => roundEnd);
+            yield return new WaitForSeconds(10f);
         }
-        yield return new WaitUntil(() => roundEnd);
-        yield return new WaitForSeconds(10f);
     }
     private IEnumerator RoundDetector()
     {
-        yield return new WaitForSeconds(0.5f);
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Monster");
-        int count = enemies.Length;
-        if (count > 0) {
-             roundEnd = false;
-        } else {
-            roundEnd = true;
+        // if any monster remains, do not continue rounds
+        while (true) {
+            yield return new WaitForSeconds(0.5f);
+            GameObject enemy = GameObject.FindGameObjectWithTag("Monster");
+            if (enemy) {
+                roundEnd = false;
+            } else {
+                roundEnd = true;
+            }
         }
     }
 }
